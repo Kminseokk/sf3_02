e70a2d8aa4b6a09a2bb0d42163e1537de250ae51
kms <highcw@naver.com>
2023-09-15 14:25:34 +0900

    project1, 2 source

---
+++ project_01/project_01.cpp
@@ -0,0 +1,68 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//      :: C++ 프로젝트 01 번 문제 ::
+//      서로 최소 1개에서 최대 3개의 연속된 숫자를 말하고 
+//      31이라는 숫자를 말하는 사람이 지는 게임.
+//          1) 사용자는 본인이 입력한 숫자만큼.
+//          2) 컴퓨터는 랜덤 숫자만큼.
+//          3) 단 사용자, 컴퓨터 모두 1~3사이의 개수만 가능
+//
+///////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0; //31이 되면 
+    int User_num, Computer_num; //입력 변수 값, 컴퓨터 값.
+    bool User_turn = true; //사용자의 차례인지 결정해줌.
+
+    cout << "나는 별로 안좋아하는 베스킨~ 라빈스~ 써리~ 원~ " << endl;
+
+    while (sum < 31) {
+        if (User_turn) {
+            cout << "사용자 차례입니다. 1~3 사이의 숫자를 입력하세요: ";
+            cin >> User_num;
+
+            cout << "사용자가 말한 숫자!!!! " << endl;
+
+            if (User_num < 1 || User_num > 3) {
+                cout << "1에서 3 사이의 숫자만 입력할 수 있습니다." << endl;
+                continue;
+            }
+            else {
+                for (int i = 0; i < User_num; i++) {
+                    sum += 1;
+                    cout << sum << endl; 
+                    // 한개씩 올라가듯이 출력해야 하므로 반복문에
+                    // sum+1을 대입하며 하나씩 출력한다.
+                }
+            }            
+        }
+        else {
+            Computer_num = rand() % 3 + 1;
+            cout << "\n컴퓨터가 " << Computer_num << "개 를 말했습니다." << endl;
+            cout << "사용자가 말한 숫자!!!! " << endl;
+
+            for (int i = 0; i < Computer_num; i++) {
+                sum += 1;
+                cout << sum << endl;
+            }
+        }
+        User_turn = !User_turn; 
+        //트루와 참을 번갈아 가면서 값을 넣어서 사용자 컴퓨터 차례를 번갈아가며 진행.
+
+    }
+
+    cout << "\n";
+    if (User_turn) {
+        cout << "게임종료! 사용자 승리!" << endl;
+    }
+    else {
+        cout << "게임종료! 컴퓨터 승리!" << endl;
+    }
+
+}
+
+
---
+++ project_02/project_02.cpp
@@ -0,0 +1,80 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//      :: C++ 프로젝트 02 번 문제 ::
+//      타임어택 끝말잇기 게임
+//      1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
+//      2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
+//      3) 단, 한번 입력된 단어는 입력하지 못하게 하기
+//
+///////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    const int timeLimit = 30; // 30초 제한을 걸어둘 조건
+    time_t startTime = time(nullptr);
+    string lastWord = "airplane"; // 게임 시작 단어를 정의합니다.
+    vector<string> usedWords; // 사용된 단어 목록을 저장하는 벡터
+
+    usedWords.push_back(lastWord);
+    cout << "시작 단어: " << lastWord << endl;
+    cout << "\n";
+
+    while (true) {
+        time_t currentTime = time(nullptr);
+        int elapsedTime = currentTime - startTime;
+
+        if (elapsedTime >= timeLimit) {
+            cout << "시간이 초과되었습니다!" << endl;
+            break;
+        }
+
+        for (int i = 0; i < usedWords.size(); i++) {
+            cout << usedWords[i];
+            if (i < usedWords.size() - 1) {
+                cout << " > ";
+            }
+        }
+
+        cout << "\n낱말을 입력하세요: ";
+
+        string inputWord;
+        cin >> inputWord;
+
+        cout << "\n";
+
+        if (inputWord.empty() || inputWord.front() != lastWord.back()) {
+            cout << "올바른 단어를 입력하세요!" << endl;
+            continue;
+        }
+
+        // 단어 목록을 직접 확인하여 중복된 단어를 검사합니다.
+        bool isRepeated = false;
+        for (const string& usedWord : usedWords) {
+            if (inputWord == usedWord) {
+                isRepeated = true;
+                break;
+            }
+        }
+
+        if (isRepeated) {
+            cout << "이미 사용된 단어입니다!" << endl;
+            continue;
+        }
+
+        // 게임에서 사용할 추가적인 단어 유효성 검사를 수행할 수 있습니다.
+
+        usedWords.push_back(inputWord); // 사용된 단어를 목록에 추가
+        lastWord = inputWord;
+    }
+
+
+}
