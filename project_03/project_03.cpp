61ba87eeee614d35525cf4671cfeeb2efb92b2f8
kms <highcw@naver.com>
2023-09-15 15:55:10 +0900

    project 3, User&Com number create

--- project_03/project_03.cpp
+++ project_03/project_03.cpp
@@ -9,9 +9,69 @@
 ///////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm> 
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello Worlasdfdsafsdafdsafd!\n";
+    srand(time(NULL));
+
+	int Computer_num[6];
+	int check[46] = { 0, };
+	int num;
+	vector<int> User_number;
+
+
+	for (int i = 0; i < 6; i++) {
+		do {
+			num = std::rand() % 45 + 1;
+		} while (check[num] == 1);
+		Computer_num[i] = num;
+		check[num] = 1;
+	} //랜덤한 중복 제외의 6개의 숫자 생성
+
+	
+	while (User_number.size() < 6 ){
+		int num;
+		bool isDuplicate = false;
+
+		// 사용자로부터 숫자 입력 받기
+		cout << "숫자를 입력하세요: ";
+		cin >> num;
+		cout << "\n";
+
+		// 중복 확인
+		if (find(User_number.begin(), User_number.end(), num) != User_number.end()) {
+			cout << "이미 입력한 숫자입니다. 다시 입력하세요." << endl;
+			isDuplicate = true;
+		}
+
+		// 중복된 숫자가 아닌 경우에만 벡터에 추가
+		if (!isDuplicate) {
+			User_number.push_back(num);
+		}
+	}
+
+
+	cout << "\n사용자의 입력 번호 !" << endl;
+	for (auto n : User_number) {
+		cout << n << " ";
+	}
+	cout << "\n";
+
+	cout << "\n당첨 번호 공개 !" << endl;
+	for (auto n : Computer_num) {
+		cout <<  n << " ";
+	}
+	cout << "\n";
+
+	cout <<  "\n1 ~ 7등 까지의 결과가 나올 수 있습니다. " << " ";
+
+
+
 }
 
